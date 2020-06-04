abd5ad12ada4fd69711a41b1ac6d02d88acb2a71
Omer Gronich <gronichomer@gmail.com>
2020-06-04 15:53:20 +0300

    Updates

---
+++ precache-manifest.e5ab1e34fd8645b7c7b2f18ce6b886c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ef008962d0bc4be166b76e35309ef1",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "d4680090ebafa7f59bfc",
+    "url": "/todo-app-react/static/css/main.66fd5732.chunk.css"
+  },
+  {
+    "revision": "d1d33f7bb5cba6ecd2d7",
+    "url": "/todo-app-react/static/js/2.1dc5d9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.1dc5d9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4680090ebafa7f59bfc",
+    "url": "/todo-app-react/static/js/main.5c9e8b06.chunk.js"
+  },
+  {
+    "revision": "004527bd9020de16f64f",
+    "url": "/todo-app-react/static/js/runtime-main.f6d63a1a.js"
+  }
+]);
